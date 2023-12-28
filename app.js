9513f6a30e2404aacdc502f2f56777dad4f0c965
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-12-28 22:34:12 +0100

    Pagina principal , Version-1

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+//Configurar Pug como motor de plantillas
+app.set('view engine', 'pug');
+app.set('views', './views')
 
-    res.send('Hello World')
-})
+//Servir Plantillas Pug desde Servidor Express:
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Golden Bakery Shop' });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`El servidor esta escuchando en el puerto ${process.env.PORT}`)
