e06711fd0a4997b3353fb005917091eef6cc4c78
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2024-01-02 12:50:38 +0100

    conectado con la base de datos mediante biblioteca mongoose con la base de datos MongoDB

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
 const app = express()
 
-
+const { dbConection } = require('./database/configDB');
 
 //Rutas
 app.use(require('./routes/web.routes'))
+
+      /*Midellwar*/
+// Configuración de Archivos estaticos
+app.use(express.static('img'));
+app.use(express.static('public'));
+//Parsear  el cuerpo de las peticiones JSON
+app.use(express.json());
 
 //Configurar Pug como motor de plantillas
 app.set('view engine', 'pug');
 app.set('views', './views')
 
-// Configuración de Archivos estaticos
-app.use( express.static('img'));
-app.use(express.static('public'));
+
 
 //Inialización de servidor
 app.listen(process.env.PORT, () => {
     console.log(`El servidor esta escuchando en el puerto ${process.env.PORT}`)
 });
+
+//Conectar base de datos
+dbConection();
---
+++ database/configDB.js
@@ -0,0 +1,19 @@
+const mongoose = require ('mongoose');
+
+const dbConection = async() => {
+    
+    try {
+
+        await mongoose.connect(process.env.MONGO_DB_ATLAS)
+
+        console.log('Base de datos online')
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error ('Error a la hora de iniciar la base de datos');
+    }
+}
+
+module.exports = {
+    dbConection
+}
